37960aa7ceed229e1777ce167d7bbd920833934e
chw <1577161287@qq.com>
2023-01-18 00:31:35 +0800

    666

---
+++ src/main/java/com/example/raj1/Raj1Application.java
@@ -0,0 +1,13 @@
+package com.example.raj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Raj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/raj1/Raj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Raj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
